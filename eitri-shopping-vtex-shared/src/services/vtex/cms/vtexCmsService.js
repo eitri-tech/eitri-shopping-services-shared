15ef84ae6fd36f3206cb5326b0f206a6222b9aef
Leonardo <leonardo.silva@calindra.com.br>
2024-09-24 15:55:44 -0300

    melhorando CMS Vtex

--- eitri-shopping-vtex-shared/src/services/vtex/cms/vtexCmsService.js
+++ eitri-shopping-vtex-shared/src/services/vtex/cms/vtexCmsService.js
@@ -1,28 +1,41 @@
-import Eitri from 'eitri-bifrost'
 import objectToQueryString from '../_helpers/objectToQueryString'
 import Vtex from '../../Vtex'
 import VtexCaller from '../_helpers/_vtexCaller'
 
 export default class VtexCmsService {
+
 	static async getAllContentTypes(projectId) {
-		const { account } = Vtex.configs
-		const BASE_URL = `https://${account}.myvtex.com`
+		const { cmsUrl, account } = Vtex.configs
+
+		let BASE_URL = `https://${account}.myvtex.com`
+		if (cmsUrl) {
+			BASE_URL = cmsUrl
+		}
 
 		const result = await VtexCaller.get(`/_v/cms/api/${projectId}`, {}, BASE_URL)
 		return result?.data
 	}
 
 	static async getPagesByContentTypes(projectId, contentTypeId, options) {
-		const { faststore } = Vtex.configs
-		const BASE_URL = `https://euentregobr.myvtex.com`
+		const { cmsUrl, account } = Vtex.configs
+
+		let BASE_URL = `https://${account}.myvtex.com`
+		if (cmsUrl) {
+			BASE_URL = cmsUrl
+		}
+
 		const queryString = objectToQueryString(options)
-		const result = await VtexCaller.get(`/_v/cms/api/${faststore}/${contentTypeId}?${queryString}`, {}, BASE_URL)
+		const result = await VtexCaller.get(`/_v/cms/api/${projectId}/${contentTypeId}?${queryString}`, {}, BASE_URL)
 		return result?.data
 	}
 
 	static async getCmsPage(projectId, contentTypeId, documentId, options) {
-		const { account } = Vtex.configs
-		const BASE_URL = `https://${account}.myvtex.com`
+		const { cmsUrl, account } = Vtex.configs
+
+		let BASE_URL = `https://${account}.myvtex.com`
+		if (cmsUrl) {
+			BASE_URL = cmsUrl
+		}
 
 		const queryString = objectToQueryString(options)
 		const result = await VtexCaller.get(
