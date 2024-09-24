f253578ccd4bea31906596ea75ebb6fb237cf6e3
Wagner Felipe <wagner.quirino@calindra.com.br>
2024-09-24 13:46:34 -0300

    mudanca arquitetura

---
+++ eitri-shopping-deco-shared/eitri-app.conf.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "name": "eitri-shopping-deco-shared",
+  "title": "eitri-shopping-deco-shared",
+  "slug": "eitri-shopping-deco-shared",
+  "eitri-luminus": "1.65.6",
+  "eitri-bifrost": "2.9.0",
+  "eitri-commons": "2.1.2",
+  "version": "0.1.0",
+  "public-key": "1d81b305-12a3-40e0-87ed-8713bf9067ae",
+  "applicationId": "99c18c0a-4112-4937-b27c-802d03f4e9e9",
+  "id": "9248e691-201b-486f-83ba-c1822cba8d26",
+  "organizationId": "cf5660ee-bf90-42cd-9a43-9d2c69ee3c89",
+  "organization": {}
+}
---
+++ eitri-shopping-wake-shared/eitri-app.conf.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "name": "eitri-shopping-wake-shared",
+  "title": "eitri-shopping-wake-shared",
+  "slug": "eitri-shopping-wake-shared",
+  "eitri-luminus": "1.65.6",
+  "eitri-bifrost": "2.9.0",
+  "eitri-commons": "2.1.2",
+  "version": "0.1.0",
+  "public-key": "1c832aa6-6172-4640-82b1-0708d8aaf4fd",
+  "applicationId": "99c18c0a-4112-4937-b27c-802d03f4e9e9",
+  "id": "0682168b-1de8-40b6-90e9-284091c4bb0b",
+  "organizationId": "cf5660ee-bf90-42cd-9a43-9d2c69ee3c89",
+  "organization": {}
+}
