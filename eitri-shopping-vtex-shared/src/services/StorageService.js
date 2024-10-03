a66a5e381cee8eb544ffd10ca9d38d40c5804d22
Wagner Felipe <wagner.quirino@calindra.com.br>
2024-10-03 11:39:10 -0300

    adiciona account nas chaves do shared

--- eitri-shopping-vtex-shared/src/services/StorageService.js
+++ eitri-shopping-vtex-shared/src/services/StorageService.js
@@ -1,27 +1,36 @@
 import Eitri from 'eitri-bifrost'
 import Logger from './Logger'
+import Vtex from "./Vtex";
 
 export default class StorageService {
 	static async setStorageItem(key, item) {
-		return Eitri.sharedStorage.setItem(key, item)
+    const account = Vtex.configs.account
+    const _key = `${account}_${key}`
+		return Eitri.sharedStorage.setItem(_key, item)
 	}
 
 	static async getStorageItem(key) {
-		return Eitri.sharedStorage.getItem(key)
+    const account = Vtex.configs.account
+    const _key = `${account}_${key}`
+		return Eitri.sharedStorage.getItem(_key)
 	}
 
 	static async setStorageJSON(key, item) {
 		try {
-			Logger.info('StorageService', 'setStorageJSON', 'Setting storage item', { key, item })
-			return Eitri.sharedStorage.setItem(key, JSON.stringify(item))
+      const account = Vtex.configs.account
+      const _key = `${account}_${key}`
+			Logger.info('StorageService', 'setStorageJSON', 'Setting storage item', { key: _key, item })
+			return Eitri.sharedStorage.setItem(_key, JSON.stringify(item))
 		} catch (e) {
 			console.error('Erro ao salvar item no storage', e)
 		}
 	}
 
 	static async getStorageJSON(key) {
-		const data = await Eitri.sharedStorage.getItem(key)
-		Logger.info('StorageService', 'getStorageJSON', 'Getting storage item', key, data)
+    const account = Vtex.configs.account
+    const _key = `${account}_${key}`
+		const data = await Eitri.sharedStorage.getItem(_key)
+		Logger.info('StorageService', 'getStorageJSON', 'Getting storage item', _key, data)
 		if (data) {
 			try {
 				return JSON.parse(data)
@@ -35,6 +44,8 @@ export default class StorageService {
 	}
 
 	static async removeItem(key) {
-		return await Eitri.sharedStorage.removeItem(key)
+    const account = Vtex.configs.account
+    const _key = `${account}_${key}`
+		return await Eitri.sharedStorage.removeItem(_key)
 	}
 }
