cfbd9e548a999b51198f9b06063f8722941ec331
Leonardo <leonardo.silva@calindra.com.br>
2025-02-20 06:45:49 -0300

    Merge branch 'main' into log-appsflyer

--- eitri-shopping-vtex-shared/src/views/CustomerMethods.jsx
+++ eitri-shopping-vtex-shared/src/views/CustomerMethods.jsx
@@ -1,4 +1,6 @@
 import Vtex from "../services/Vtex";
+import StorageService from "../services/StorageService";
+import VtexCustomerService from "../services/vtex/customer/vtexCustomerService";
 
 export default function CustomerMethods() {
 
@@ -21,6 +23,11 @@ export default function CustomerMethods() {
     console.log('isLogged', logged)
   }
 
+  const getMyStoredToken = async () => {
+    const savedToken = await Vtex.customer.getStorageCustomerToken()
+    console.log('savedToken', savedToken)
+  }
+
   return (
     <Window topInset bottomInset>
       <View padding='large' direction='column' gap={10} justifyContent='center' alignItems='center' width='100%'>
@@ -32,6 +39,9 @@ export default function CustomerMethods() {
         <View display='flex' gap={10}>
           <Input placeholder='Access Key' inputType='numeric' grow={4} value={accessKey} onChange={(value) => setAccessKey(value)} />
           <Button color='background-color' grow={1} onPress={loginWithEmailAndAccessKey} label='Validar login' />
+        </View>
+        <View display='flex' gap={10}>
+          <Button wide color='background-color' grow={1} onPress={getMyStoredToken} label='Meu token' />
         </View>
       </View>
     </Window>
