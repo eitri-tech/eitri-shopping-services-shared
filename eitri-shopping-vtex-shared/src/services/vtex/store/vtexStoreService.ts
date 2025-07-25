2a5cde0801ca6ef542eea60194333e4370b3d31e
Wagner Quirino <wagner.quirino@calindra.com.br>
2025-07-25 18:50:27 -0300

    update getLoginProviders to support scope and accountName parameters

--- eitri-shopping-vtex-shared/src/services/vtex/store/vtexStoreService.ts
+++ eitri-shopping-vtex-shared/src/services/vtex/store/vtexStoreService.ts
@@ -1,8 +1,26 @@
 import VtexCaller from '../_helpers/_vtexCaller'
+import Vtex from '../../Vtex'
+
+type AuthConfig = {
+	passwordAuthentication: boolean
+	accessKeyAuthentication: boolean
+	passkeyAuthentication: boolean
+	samlProviders: any[]
+	corporateProviders: any[]
+	oAuthProviders: {
+		providerName: string
+		className: string
+		expectedContext: any[]
+	}[]
+	sessionRenewal: boolean
+}
 
 export default class VtexStoreService {
-	static async getLoginProviders() {
-		const response = await VtexCaller.get(`api/vtexid/pub/authentication/providers`)
+	static async getLoginProviders(scope?: String, accountName?: String): Promise<AuthConfig> {
+		const { account } = Vtex.configs
+		const response = await VtexCaller.get(
+			`api/vtexid/pub/authentication/providers?scope=${scope || account}&accountName=${accountName || account}`
+		)
 		return response.data
 	}
 }
