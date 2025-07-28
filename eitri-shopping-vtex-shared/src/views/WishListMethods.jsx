715b67f25c27b971b87928affa803012de3d3399
Wagner Quirino <wagner.quirino@calindra.com.br>
2025-07-28 11:45:31 -0300

    fix wishlist functionality
    - Updated wishlist methods to include add, remove, and check item actions.
    - Corrected GraphQL queries by removing outdated context and locale attributes.
    - Incremented version to 1.1.6.

--- eitri-shopping-vtex-shared/src/views/WishListMethods.jsx
+++ eitri-shopping-vtex-shared/src/views/WishListMethods.jsx
@@ -12,10 +12,33 @@ export default function WishListMethods() {
 
 	const addItem = async () => {
 		try {
-			const res = await Vtex.wishlist.addItem('116141', 'Barbie Ken Conjunto de Praia - Mattel', '116141')
+			const products = await Vtex.catalog.legacyParamsSearch('fq=P:%5B0%2520TO%252099999%5D&_from=0&_to=49')
+			const product = products[Math.floor(Math.random() * products.length)]
+			const sku = product.items[0]
+			const res = await Vtex.wishlist.addItem(product.productId, product.productName, sku.itemId)
 			console.log(res)
 		} catch (e) {
 			console.error(e)
+		}
+	}
+
+	const removeItem = async () => {
+		try {
+			const itemId = '12'
+			const res = await Vtex.wishlist.removeItem(itemId)
+			console.log('Item removido:', res)
+		} catch (e) {
+			console.error('Erro ao remover item:', e)
+		}
+	}
+
+	const checkItem = async () => {
+		try {
+			const productId = '2023347'
+			const res = await Vtex.wishlist.checkItem(productId)
+			console.log('Info do item:', res)
+		} catch (e) {
+			console.error('Erro ao checar item:', e)
 		}
 	}
 
@@ -41,6 +64,18 @@ export default function WishListMethods() {
 					color='background-color'
 					onPress={addItem}
 					label='Adicionar item na minha lista'
+				/>
+				<Button
+					wide
+					color='background-color'
+					onPress={removeItem}
+					label='Remover item na minha lista'
+				/>
+				<Button
+					wide
+					color='background-color'
+					onPress={checkItem}
+					label='Checar item na minha lista'
 				/>
 			</View>
 		</Window>
