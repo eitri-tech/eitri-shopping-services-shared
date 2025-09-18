d7653445eabefd5160246fd3340c5fa6a8658ad1
Wagner Quirino <wagner.quirino@calindra.com.br>
2025-09-18 16:26:10 -0300

    Update version to 1.1.9, standardize import statements, refactor WakeService and ClarityService methods, and improve logging and error handling.

--- eitri-shopping-wake-shared/src/services/WakeService.js
+++ eitri-shopping-wake-shared/src/services/WakeService.js
@@ -1,110 +1,106 @@
-import Eitri from "eitri-bifrost";
-import ProductService from "./ProductService";
-import GraphqlService from "./GraphqlService";
-import CategoryService from "./CategoryService";
-import CartService from "./CartService";
-import CustomerService from "./CustomerService";
-import CheckoutService from "./CheckoutService";
-import ClarityService from "./tracking/ClarityService";
+import Eitri from 'eitri-bifrost'
+import ProductService from './ProductService'
+import GraphqlService from './GraphqlService'
+import CategoryService from './CategoryService'
+import CartService from './CartService'
+import CustomerService from './CustomerService'
+import CheckoutService from './CheckoutService'
+import ClarityService from './tracking/ClarityService'
 
 export default class WakeService {
-  static graphQl = GraphqlService;
-  static product = ProductService;
-  static category = CategoryService;
-  static cart = CartService;
-  static customer = CustomerService;
-  static checkout = CheckoutService;
+	static graphQl = GraphqlService
+	static product = ProductService
+	static category = CategoryService
+	static cart = CartService
+	static customer = CustomerService
+	static checkout = CheckoutService
 
-  static configs = {
-    verbose: false,
-    gaVerbose: false,
-    autoTriggerGAEvents: true,
-    clarityId: "",
-    provider: "WAKE",
-    account: "",
-    tcs_account: "",
-    graphqlApi: "",
-    host: "",
-    cartHost: "",
-    searchOptions: {},
-    segments: null,
-  };
+	static configs = {
+		verbose: false,
+		gaVerbose: false,
+		autoTriggerGAEvents: true,
+		clarityId: '',
+		provider: 'WAKE',
+		account: '',
+		tcs_account: '',
+		graphqlApi: '',
+		host: '',
+		cartHost: '',
+		searchOptions: {},
+		segments: null
+	}
 
-  static configure = async (remoteConfig) => {
-    const { providerInfo, marketingTag, ...rest } = remoteConfig;
+	static configure = remoteConfig => {
+		const { providerInfo, marketingTag, ...rest } = remoteConfig
 
-    if (providerInfo.host && !providerInfo.host.startsWith("https://")) {
-      providerInfo.host = "https://" + providerInfo.host;
-    }
-    if (
-      providerInfo.cartHost &&
-      !providerInfo.cartHost.startsWith("https://")
-    ) {
-      providerInfo.cartHost = "https://" + providerInfo.cartHost;
-    }
-    if (providerInfo.apiHost && !providerInfo.apiHost.startsWith("https://")) {
-      providerInfo.apiHost = "https://" + providerInfo.apiHost;
-    }
+		if (providerInfo.host && !providerInfo.host.startsWith('https://')) {
+			providerInfo.host = 'https://' + providerInfo.host
+		}
+		if (providerInfo.cartHost && !providerInfo.cartHost.startsWith('https://')) {
+			providerInfo.cartHost = 'https://' + providerInfo.cartHost
+		}
+		if (providerInfo.apiHost && !providerInfo.apiHost.startsWith('https://')) {
+			providerInfo.apiHost = 'https://' + providerInfo.apiHost
+		}
 
-    WakeService.configs = {
-      ...WakeService.configs,
-      ...providerInfo,
-      ...rest,
-      verbose: remoteConfig.verbose ?? false,
-      autoTriggerGAEvents: remoteConfig?.autoTriggerGAEvents ?? true,
-      graphqlApi: `https://storefront-api.fbits.net/graphql`,
-      marketingTag: marketingTag ?? "eitri-shop",
-    };
-  };
+		WakeService.configs = {
+			...WakeService.configs,
+			...providerInfo,
+			...rest,
+			verbose: remoteConfig.verbose ?? false,
+			autoTriggerGAEvents: remoteConfig?.autoTriggerGAEvents ?? true,
+			graphqlApi: `https://storefront-api.fbits.net/graphql`,
+			marketingTag: marketingTag ?? 'eitri-shop'
+		}
+	}
 
-  static tryAutoConfigure = async (overwrites) => {
-    let remoteConfig;
-    try {
-      const _remoteConfig = await Eitri.environment.getRemoteConfigs();
-      remoteConfig = { ..._remoteConfig, ...overwrites };
-    } catch (error) {
-      console.error("[SHARED] Error getRemoteConfigs", error);
-      throw error;
-    }
+	static tryAutoConfigure = async overwrites => {
+		let remoteConfig
+		try {
+			console.log('[SHARED] ********* Buscando remote config *******')
+			const _remoteConfig = await Eitri.environment.getRemoteConfigs()
+			console.log('[SHARED] ********* Remote config encontrado *******')
+			remoteConfig = { ..._remoteConfig, ...overwrites }
+		} catch (error) {
+			console.log('[SHARED] Error getRemoteConfigs', error)
+			throw error
+		}
 
-    try {
-      await WakeService.configure(remoteConfig);
-    } catch (error) {
-      console.error(
-        "[SHARED] Error autoConfigure ",
-        remoteConfig.ecommerceProvider,
-        error,
-      );
-      throw error;
-    }
+		try {
+			console.log('[SHARED] ********* Configurando variáveis globais *******')
+			WakeService.configure(remoteConfig)
+		} catch (error) {
+			console.log('[SHARED] Error autoConfigure ', remoteConfig.ecommerceProvider, error)
+			throw error
+		}
 
-    try {
-      const environment = await Eitri.environment.getName();
-      if (remoteConfig.appConfigs?.clarityId && environment === "prod") {
-        WakeService.configs.clarityId = remoteConfig.appConfigs.clarityId;
-        await ClarityService.init(remoteConfig.appConfigs.clarityId);
-      }
-    } catch (error) {
-      console.error("[SHARED] Error clarity ", remoteConfig.clarityId, error);
-      throw error;
-    }
+		try {
+			console.log('[SHARED] ********* Verificando Clarity *******')
+			const environment = await Eitri.environment.getName()
+			if (remoteConfig.appConfigs?.clarityId && environment === 'prod') {
+				ClarityService.init(remoteConfig.appConfigs.clarityId)
+			} else {
+				console.log('[SHARED] ********* Clarity Id não encontrado ou não é prod *******')
+			}
+		} catch (error) {
+			console.log('[SHARED] Error clarity ', remoteConfig.clarityId, error)
+		}
 
-    try {
-      if (remoteConfig?.appConfigs?.statusBarTextColor) {
-        const color =
-          remoteConfig.appConfigs.statusBarTextColor === "white"
-            ? "setStatusBarTextWhite"
-            : "setStatusBarTextBlack";
-        window.EITRI.connector.invokeMethod(color);
-      }
-    } catch (error) {
-      console.error("[SHARED] Error App configure ", error);
-      throw error;
-    }
+		try {
+			console.log('[SHARED] ********* Configurando status bar text color *******')
 
-    console.log(
-      `[SHARED] App WAKE ${WakeService.configs.account} configurado com sucesso`,
-    );
-    return WakeService.configs;
-  };
+			if (remoteConfig?.appConfigs?.statusBarTextColor) {
+				const color =
+					remoteConfig.appConfigs.statusBarTextColor === 'white'
+						? 'setStatusBarTextWhite'
+						: 'setStatusBarTextBlack'
+				window.EITRI.connector.invokeMethod(color)
+			}
+		} catch (error) {
+			console.log('[SHARED] Error App configure ', error)
+		}
+
+		console.log(`[SHARED] App WAKE ${WakeService.configs.account} configurado com sucesso`)
+		return WakeService.configs
+	}
 }
