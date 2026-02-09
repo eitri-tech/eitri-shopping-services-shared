366c5dc600915d6e7c783b5c1cef8111b91d972c
Wagner Quirino <wagner.quirino@calindra.com.br>
2026-02-09 15:20:29 -0300

    Update configuration to include `messageVersion` value

--- eitri-shopping-shopify-shared/eitri-app.conf.js
+++ eitri-shopping-shopify-shared/eitri-app.conf.js
@@ -5,6 +5,7 @@ module.exports = {
 	'eitri-luminus': '1.90.0',
 	'eitri-bifrost': '4.7.0',
 	'version': '0.1.0',
+	'messageVersion': 'Here we go',
 	'public-key': 'c599744e-9c7e-4159-a189-fe256560789c',
 	'applicationId': '99c18c0a-4112-4937-b27c-802d03f4e9e9',
 	'id': 'd6b471c7-c47b-4469-b380-79dcb088fd8d',
