f2c835f6896063c69c50a396942ff90c1ffd5111
Gabriel Silva <gabriel.silva@calindra.com.br>
2026-02-11 17:10:56 -0300

    chore(version): update eitri-app version

--- eitri-shopping-shopify-shared/eitri-app.conf.js
+++ eitri-shopping-shopify-shared/eitri-app.conf.js
@@ -4,7 +4,7 @@ module.exports = {
 	'slug': 'eitri-shopping-shopify-shared',
 	'eitri-luminus': '1.90.0',
 	'eitri-bifrost': '4.7.0',
-	'version': '0.2.0',
+	'version': '0.2.1',
 	'messageVersion': 'fix: use shared storage',
 	'public-key': 'c599744e-9c7e-4159-a189-fe256560789c',
 	'applicationId': '99c18c0a-4112-4937-b27c-802d03f4e9e9',
