2602b37d76469a7bd2e44947fd29b578ce92d746
Leonardo <leonardo.silva@calindra.com.br>
2026-03-06 12:01:16 -0300

    exportando metodos para requests

--- eitri-shopping-shopify-shared/src/services/Shopify.ts
+++ eitri-shopping-shopify-shared/src/services/Shopify.ts
@@ -2,10 +2,13 @@ import { CartService } from './cart/CartService'
 import CatalogService from './catalog/CatalogService'
 import { AddressService } from './AddressService'
 import { CustomerService } from './customer/CustomerService'
+import ShopifyCaller from './_helpers/ShopifyCaller'
 
 export default class Shopify {
 	static catalog = CatalogService
 	static cart = CartService
 	static address = AddressService
 	static customer = CustomerService
+	static http = ShopifyCaller
+
 }
