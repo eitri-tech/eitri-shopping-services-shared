6614b6b4f90774383072c759d1297c900101fa0e
Gabriel Accetta <gabriel.accetta@calindra.com.br>
2026-04-06 17:46:05 -0300

    refactor: update subscribeAvailability method to use GraphQL mutation for availability subscription

--- eitri-shopping-vtex-shared/src/services/vtex/availability/vtexAvailabilityService.js
+++ eitri-shopping-vtex-shared/src/services/vtex/availability/vtexAvailabilityService.js
@@ -1,41 +1,36 @@
 import VtexCaller from '../_helpers/_vtexCaller'
 import Vtex from '../../Vtex'
 
+const CREATE_CONTENT_VARIANT = 'mutation AvailabilitySubscribe($name: String, $email: String, $skuId: String, $locale: String, $sellerObj: SellerObjInputType!) @context(sender: "vtex.availability-notify@1.14.1") @runtimeMeta(hash: "d975646fd9ed900ff082021a138d3db656e9ba33844b79d84d2bba15160cdcf3") {\n  availabilitySubscribe(name: $name, email: $email, skuId: $skuId, locale: $locale, sellerObj: $sellerObj)\n}\n'
+
 export default class VtexAvailabilityService {
 
 	static async subscribeAvailability(skuId, name, email) {
+		const locale = Vtex.configs.locale
+		const sellerObj = {}
+
 		const variables = {
-			acronym: "AS",
-			document: {
-				fields: [
-					{ key: "skuId",            value: String(skuId) },
-					{ key: "name",             value: name },
-					{ key: "email",            value: email },
-					{ key: "notificationSend", value: "false" },
-					{ key: "createdAt",        value: new Date().toISOString() }
-				]
-			}
+			skuId: String(skuId),
+			name,
+			email,
+			locale,
+			sellerObj
 		}
 
 		const encodedVariables = btoa(JSON.stringify(variables))
 
-		const body = {
-			operationName: "availabilitySubscribe",
-			variables: {},
+		const graphqlBodyData = {
+            operationName: "AvailabilitySubscribe",
+            query: CREATE_CONTENT_VARIANT,
+            variables: {},
 			extensions: {
-				persistedQuery: {
-					version: 1,
-					sha256Hash: "8b059022fc39294a310133833c9a54323691d52c13767cefce52abc5d5515729",
-					sender: "vtex.store-components@3.x",
-					provider: "vtex.store-graphql@2.x"
-				},
 				variables: encodedVariables
 			}
-		}
+        };
 
 		const response = await VtexCaller.post(
-			`_v/private/graphql/v1?workspace=master&maxAge=long&appsEtag=remove&domain=store&locale=pt-BR`,
-			body,
+			`_v/private/graphql/v1`,
+			graphqlBodyData,
 			{},
 			Vtex.configs.host,
 			{ 'content-type': 'application/json' }
