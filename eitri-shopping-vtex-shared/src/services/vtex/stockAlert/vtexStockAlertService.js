8d4b7cdddf0273b61dc017e237f84860c3a278a2
Gabriel Accetta <gabriel.accetta@calindra.com.br>
2026-04-08 18:33:26 -0300

    refactor: remove unnecessary runtimeMeta and content-type header in subscribeAvailability method

--- eitri-shopping-vtex-shared/src/services/vtex/stockAlert/vtexStockAlertService.js
+++ eitri-shopping-vtex-shared/src/services/vtex/stockAlert/vtexStockAlertService.js
@@ -4,7 +4,7 @@ import Vtex from '../../Vtex'
 export default class VtexStockAlertService {
 
 	static async subscribeAvailability(skuId, name, email, sellerId, sellerName) {
-		const SUBSCRIBE_AVAILABILITY_MUTATION = 'mutation AvailabilitySubscribe($name: String, $email: String, $skuId: String, $locale: String, $sellerObj: SellerObjInputType!) @context(sender: "vtex.availability-notify@1.14.1") @runtimeMeta(hash: "d975646fd9ed900ff082021a138d3db656e9ba33844b79d84d2bba15160cdcf3") {\n  availabilitySubscribe(name: $name, email: $email, skuId: $skuId, locale: $locale, sellerObj: $sellerObj)\n}\n'
+		const SUBSCRIBE_AVAILABILITY_MUTATION = 'mutation AvailabilitySubscribe($name: String, $email: String, $skuId: String, $locale: String, $sellerObj: SellerObjInputType!) @context(sender: "vtex.availability-notify@1.14.1") {\n  availabilitySubscribe(name: $name, email: $email, skuId: $skuId, locale: $locale, sellerObj: $sellerObj)\n}\n'
 		const locale = Vtex.configs?.locale
 		const sellerObj = {
 			sellerId: sellerId ?? '',
@@ -35,7 +35,6 @@ export default class VtexStockAlertService {
 			graphqlBodyData,
 			{},
 			Vtex.configs.host,
-			{ 'content-type': 'application/json' }
 		)
 
 		return response.data
