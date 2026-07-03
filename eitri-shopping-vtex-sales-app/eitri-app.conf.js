3769697cdf233e043ba741b3174b3bf89688b441
Gabriel Silva <gabriel.silva@calindra.com.br>
2026-07-03 15:38:19 -0300

    Refactor code structure for improved readability and maintainability

--- eitri-shopping-vtex-sales-app/eitri-app.conf.js
+++ eitri-shopping-vtex-sales-app/eitri-app.conf.js
@@ -5,7 +5,7 @@ module.exports = {
 	'eitri-luminus': '1.90.0',
 	'eitri-bifrost': '5.4.0',
 	'eitri-commons': '2.2.1',
-	'version': '0.2.1',
+	'version': '0.2.2',
 	'messageVersion': 'primeira versão do sdk de vendas assistidas (instore)',
 	'id': '528244e2-517f-4203-a9ca-0e3ab4550c19',
 	'eitri-app-dependencies': {
