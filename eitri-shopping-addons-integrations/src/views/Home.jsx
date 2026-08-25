a5c5e9b00efc6aaa3b494d21a90fd3c0690211e9
Hudson Rodrigues <hudson.rodrigues@calindra.com.br>
2026-08-25 14:40:51 -0300

    fix: luminus version

--- eitri-shopping-addons-integrations/src/views/Home.jsx
+++ eitri-shopping-addons-integrations/src/views/Home.jsx
@@ -1,19 +1,33 @@
 import Eitri from 'eitri-bifrost'
 
 export default function Home() {
+	const navigateTo = async path => {
+		Eitri.navigation.navigate({ path })
+	}
 
-    const navigateTo = async path => {
-        Eitri.navigation.navigate({ path })
-    }
-
-return (
-	<View className='min-h-screen flex flex-col items-center justify-center gap-10 p-8'>
-		<Text className='text-xl font-bold'>Addons & Integrações</Text>
-		<Button
-			className='btn-neutral w-full'
-			onClick={() => navigateTo('WiddeMethods')}>
-			Métodos Widde
-		</Button>
-	</View>
-)
+	return (
+		<Window
+			topInset
+			bottomInset>
+			<View
+				padding='large'
+				direction='column'
+				gap={16}
+				justifyContent='center'
+				alignItems='center'
+				width='100%'>
+				<Text
+					fontSize='large'
+					fontWeight='bold'>
+					Addons & Integrações
+				</Text>
+				<Button
+					wide
+					color='background-color'
+					onPress={() => navigateTo('WiddeMethods')}
+					label='Métodos Widde'
+				/>
+			</View>
+		</Window>
+	)
 }
